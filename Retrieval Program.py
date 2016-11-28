80420c9d7dca34a65f64e9aec661c6290f2a52a9
TheGraeme95 <graeme_96@hotmail.co.uk>
2016-11-28 18:45:02 +0000

    It writes one stock to local SQL
    It writes one stock to local SQL database, need to do rest of stocks

--- Retrieval Program.py
+++ Retrieval Program.py
@@ -6,13 +6,20 @@ import quandl
 import pandas as pd
 import os
 import time
+from sqlalchemy import create_engine
+import mysql.connector
 from datetime import datetime
+
 quandl.ApiConfig.api_key = "p_qounXgMs57T9nYAurW"
 
 token = 'p_qounXgMs57T9nYAurW'
 start = '2016-01-01'
 stocklist = ['L_IBM','AAPL','MSFT','GOOGL','DELL','','','','','','','','','','']
+#cnx = mysql.connector.connect(user='root', password='machinelearning1', host='localhost', database='stockdata')
+engine = create_engine('mysql+mysqlconnector://root:machinelearning1@localhost:3306/stockdata')
+dbschema = 'stockdata'
 
 
 df = quandl.get("YAHOO/L_IBM",trim_start = start, authtoken=token)
 print(df)
+df.to_sql('l_ibm', con = engine, schema = dbschema, if_exists = 'append',index=True)
