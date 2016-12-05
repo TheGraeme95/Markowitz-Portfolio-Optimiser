4fe5b372a780f979094ba6a91b34b7781cf302ff
TheGraeme95 <graeme_96@hotmail.co.uk>
2016-12-05 19:35:39 +0000

    Changed to new online database

--- Retrieval Program.py
+++ Retrieval Program.py
@@ -15,14 +15,15 @@ quandl.ApiConfig.api_key = "p_qounXgMs57T9nYAurW"
 token = 'p_qounXgMs57T9nYAurW'
 start = '2016-01-01'
 stocklist = ['l_ibm','aapl','msft','googl']
-engine = create_engine('mysql+mysqlconnector://root:machinelearning1@localhost:3306/stockdata')
-dbschema = 'stockdata'
+#engine = create_engine('mysql+mysqlconnector://root:machinelearning1@localhost:3306/stockdata')
+engine = create_engine('mysql+mysqlconnector://TheGraeme95:machinelearning1@stockdatadb.czl4fjyxavu7.eu-west-1.rds.amazonaws.com:3306/Stockdata')
+dbschema = 'Stockdata'
 
 for stock in stocklist:
     df = quandl.get("YAHOO/"+ stock,trim_start = start, authtoken=token)
     print(stock)
     print(df)
-    df.to_sql(stock, con = engine, schema = dbschema, if_exists = 'append',index=True)
+    df.to_sql(stock, con = engine, schema = dbschema, if_exists = 'replace',index=True)
 
 #df = quandl.get("YAHOO/l_ibm",trim_start = start, authtoken=token)
 #print(df)
