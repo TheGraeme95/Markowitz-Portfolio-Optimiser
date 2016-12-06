0262d96a944d5d0911a2f37db7d4f5c38decc84b
TheGraeme95 <graeme_96@hotmail.co.uk>
2016-12-06 01:16:38 +0000

    Fixed LBM stock

--- Retrieval Program.py
+++ Retrieval Program.py
@@ -14,8 +14,7 @@ quandl.ApiConfig.api_key = "p_qounXgMs57T9nYAurW"
 
 token = 'p_qounXgMs57T9nYAurW'
 start = '2016-01-01'
-stocklist = ['l_ibm','aapl','msft','googl']
-#engine = create_engine('mysql+mysqlconnector://root:machinelearning1@localhost:3306/stockdata')
+stocklist = ['ibm','aapl','msft','googl']
 engine = create_engine('mysql+mysqlconnector://TheGraeme95:machinelearning1@stockdatadb.czl4fjyxavu7.eu-west-1.rds.amazonaws.com:3306/Stockdata')
 dbschema = 'Stockdata'
 
