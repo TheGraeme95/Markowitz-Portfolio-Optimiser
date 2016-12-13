12c692f6ebc59fdc88067969c82550777274a45a
TheGraeme95 <graeme_96@hotmail.co.uk>
2016-12-13 04:55:30 +0000

    Added name column
    Added the name column to each different stock

--- Retrieval Program.py
+++ Retrieval Program.py
@@ -9,6 +9,7 @@ import time
 from sqlalchemy import create_engine
 import mysql.connector
 from datetime import datetime
+import Schedule
 
 quandl.ApiConfig.api_key = "p_qounXgMs57T9nYAurW"
 
@@ -18,11 +19,20 @@ stocklist = ['ibm','aapl','msft','googl']
 engine = create_engine('mysql+mysqlconnector://TheGraeme95:machinelearning1@stockdatadb.czl4fjyxavu7.eu-west-1.rds.amazonaws.com:3306/Stockdata')
 dbschema = 'Stockdata'
 
-for stock in stocklist:
-    df = quandl.get("YAHOO/"+ stock,trim_start = start, authtoken=token)
-    print(stock)
-    print(df)
-    df.to_sql(stock, con = engine, schema = dbschema, if_exists = 'replace',index=True)
+def GetStocks():
+    for stock in stocklist:
+        df = quandl.get("YAHOO/"+ stock,trim_start = start, authtoken=token)
+        df['Name']=stock
+        print(stock)
+        print(df)
+        df.to_sql(stock, con = engine, schema = dbschema, if_exists = 'replace',index=True)
+
+schedule.every().day.at("04:30").do(GetStocks)
+
+while True:
+    schedule.run_pending()
+    time.sleep(60)
+
 
 #df = quandl.get("YAHOO/l_ibm",trim_start = start, authtoken=token)
 #print(df)
