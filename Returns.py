9ba5895dcb88e867dbaf9e6cc19350110cf48a64
TheGraeme95 <graeme_96@hotmail.co.uk>
2017-02-07 05:24:48 +0000

    edits

--- Returns.py
+++ Returns.py
@@ -2,23 +2,25 @@ import numpy
 import os
 import pandas as pd
 import matplotlib
+import matplotlib.pyplot as plt
 from sqlalchemy import create_engine
 
 engine = create_engine('mysql+mysqlconnector://TheGraeme95:machinelearning1@stockdatadb.czl4fjyxavu7.eu-west-1.rds.amazonaws.com:3306/Stockdata')
 
 stocklist = ['ibm','aapl','msft','googl']
-returnColumns = ['Adj. Close']
 dbSchema = 'Stockdata'
 
 class Stock:
     def __init__(self,name):
         try:
             self.name = name
-            self.close = pd.read_sql_table(name, con = engine, schema = dbSchema, index_col = 'Date' , columns = returnColumns)
+            self.Data = pd.read_sql_table(name, con = engine, schema = dbSchema, index_col = 'Date')                       
         except Exception as e:
             print("Not a valid stock name in the WIKI dataset.", str(e))
             
             
+#IBM = Stock('ibm')
+#IBMprices = IBM.close.pct_change(1)
+#print(IBMprices)
 IBM = Stock('ibm')
-print(IBM.close)
-print(IBM.prices_change(252))
+print(IBM.Data['Adj. Close'].pct_change(1))
