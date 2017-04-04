2867e96938bf95bbdbaf984f108f0f7dbb9ce9f8
TheGraeme95 <graeme_96@hotmail.co.uk>
2017-04-04 18:14:00 +0100

    Redoing Gui

--- Gui/First Attempt.py
+++ Gui/First Attempt.py
@@ -1,18 +1,60 @@
 import sys
-from PyQt5.QtWidgets import QMainWindow, QApplication
-from PyQt5 import uic
-qtCreatorFile = "MainWindow.ui" # Enter file here.
+from PyQt5.QtWidgets import QMainWindow, QApplication, qApp, QWidget
+from PyQt5 import uic, QtCore, QtGui
  
-Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
+Ui_MainWindow = uic.loadUiType("MainWindow.ui")[0]
+Ui_StockChooser = uic.loadUiType("StockChooser.ui")[0]
+Ui_StockPerformance = uic.loadUiType("StockPerformance.ui")[0]
+Ui_StockAnalysis = uic.loadUiType("StockAnalysis.ui")[0]
  
-class MyApp(QMainWindow):
+class MainWindow(QMainWindow):
     def __init__(self):
-        super(MyApp, self).__init__()
+        super(MainWindow, self).__init__()
         self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
+        self.ui.setupUi(self)      
+        self.ui.actionClose.triggered.connect(qApp.quit)
+        self.ui.actionChoose_Stocks.triggered.connect(self.openStockChooser)
+        self.ui.actionStock_Performance.triggered.connect(self.openStockPerformance)        
+        self.ui.actionStock_Analysis.triggered.connect(self.openStockAnalysis)
+        
+    
+    def openStockChooser(self):
+        self.myStockChooser = StockChooser()
+        self.myStockChooser.show()
+    
+    def openStockPerformance(self):
+        self.myStockPerformance = StockPerformance()
+        self.myStockPerformance.show()
+    
+    def openStockAnalysis(self):
+        self.myStockAnalysis = StockAnalysis()
+        self.myStockAnalysis.show()      
+    
  
+
+class StockChooser(QWidget):
+    def __init__(self):
+        super(StockChooser, self).__init__()
+        self.ui = Ui_StockChooser()
+        self.ui.setupUi(self)       
+    
+
+class StockPerformance(QWidget):
+    def __init__(self):
+        super(StockPerformance, self).__init__()
+        self.ui = Ui_StockPerformance()
+        self.ui.setupUi(self)
+        
+class StockAnalysis(QWidget):
+    def __init__(self):
+        super(StockAnalysis, self).__init__()
+        self.ui = Ui_StockAnalysis()
+        self.ui.setupUi(self)
+        
+
+        
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = MyApp()
+    window = MainWindow()
     window.show()
     sys.exit(app.exec_())
